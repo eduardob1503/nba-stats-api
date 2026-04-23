9ac9b12906b7f39264bdc77aefe1ff46d0bdbc58
Eduardo Barcelos VIana <eticleo@yahoo.com.br>
2026-04-23 14:14:14 -0300

    render+local

--- config.py
+++ config.py
@@ -2,11 +2,5 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = {
-    "host":os.getenv("host"),
-    "port": os.getenv("port"),
-    "user": os.getenv("user"),
-    "password": os.getenv("password"),
-    "database": os.getenv("database")
-}
+
 SECRET_KEY = os.getenv("SECRET_KEY") 
